0387ae69014b47b4f8b30a225c3d1e9bc3ae142f
Lyarkh <lcemanuel.emanuel@gmail.com>
2021-12-23 11:20:08 -0300

    Usando Padrão Factory

--- src/main.py
+++ src/main.py
@@ -1,10 +1,9 @@
 #Testes das classes criadas
-from fila_normal import FilaNormal
-from fila_prioritaria import FilaPrioritaria
+from fabrica_fila import FabricaFila
 
 
 print("--------Teste_01--------")
-fila_teste = FilaNormal()
+fila_teste = FabricaFila.pega_fila("NORMAL")
 fila_teste.atualiza_fila()
 fila_teste.atualiza_fila()
 fila_teste.atualiza_fila()
@@ -16,7 +15,7 @@ print(fila_teste.chama_cliente(5))
 print(fila_teste.chama_cliente(10))
 
 print("\n--------Teste_02--------")
-fila_teste_2 = FilaPrioritaria()
+fila_teste_2 = FabricaFila.pega_fila("PRIORITARIA")
 fila_teste_2.atualiza_fila()
 fila_teste_2.atualiza_fila()
 fila_teste_2.atualiza_fila()
