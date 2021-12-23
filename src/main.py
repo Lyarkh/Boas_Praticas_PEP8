8a31f33b62a96418744aaef6b21d01111eb03ad1
Lyarkh <lcemanuel.emanuel@gmail.com>
2021-12-23 09:36:54 -0300

    Classe Fila Prioritaria seguindo gia PEP-8

--- src/main.py
+++ src/main.py
@@ -1,6 +1,9 @@
 #Testes das classes criadas
 from fila_normal import filanormal
+from fila_prioritaria import FilaPrioritaria
 
+""" 
+--------Teste_01--------
 fila_teste = filanormal()
 fila_teste.atualizafila()
 fila_teste.atualizafila()
@@ -10,4 +13,19 @@ fila_teste.atualizafila()
 fila_teste.atualizafila()
 
 print(fila_teste.chamacliente(5))
-print(fila_teste.chamacliente(10))
+print(fila_teste.chamacliente(10)) """
+
+
+#--------Teste_02--------
+fila_teste_2 = FilaPrioritaria()
+fila_teste_2.atualiza_fila()
+fila_teste_2.atualiza_fila()
+fila_teste_2.atualiza_fila()
+fila_teste_2.atualiza_fila()
+
+print(fila_teste_2.chama_cliente(4))
+print(fila_teste_2.chama_cliente(1))
+print(fila_teste_2.chama_cliente(5))
+print(fila_teste_2.chama_cliente(7))
+
+print(fila_teste_2.estatistica("10/01/1993", 198, "detail"))
