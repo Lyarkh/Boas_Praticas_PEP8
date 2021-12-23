85623424d3ff6138bf169ca914d9a6a0f3b3b476
Lyarkh <lcemanuel.emanuel@gmail.com>
2021-12-23 09:16:51 -0300

    colocando classe fila_normal sem formatação.

---
+++ src/main.py
@@ -0,0 +1,13 @@
+#Testes das classes criadas
+from fila_normal import filanormal
+
+fila_teste = filanormal()
+fila_teste.atualizafila()
+fila_teste.atualizafila()
+fila_teste.atualizafila()
+fila_teste.atualizafila()
+fila_teste.atualizafila()
+fila_teste.atualizafila()
+
+print(fila_teste.chamacliente(5))
+print(fila_teste.chamacliente(10))
